574dae6d063cd702489ddf05539ab26d2b3d7e26
Abhishek Gangwar <me.abhishekgangwar>
2024-05-04 16:28:14 +0530

    tab navigator logic

--- machine-coding-html-css-js/tab-navigator/index.js
+++ machine-coding-html-css-js/tab-navigator/index.js
@@ -1,3 +1,26 @@
 class TabNavigator {}
 
-function handleNavItemClick() {}
+function registerEventListener(clickCallback) {
+  const navElement = document.getElementById("navigation");
+
+  navElement.addEventListener("click", (e) => {
+    clickCallback(e);
+  });
+}
+
+function handleItemClick(e) {
+  const clickedItem = e.target;
+  const contentElements = document.getElementsByClassName("content");
+
+  for (let element of contentElements) {
+    const contentIdValue = element.attributes["content-id"].value;
+
+    if (contentIdValue === clickedItem.id) {
+      element.classList.add("content-active");
+    } else {
+      element.classList.remove("content-active");
+    }
+  }
+}
+
+registerEventListener(handleItemClick);
